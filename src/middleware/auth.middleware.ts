9371f046d603e6051ede4b9dae6ad213a914647e
jonasliendl <57270170+jonasliendl@users.noreply.github.com>
2024-02-17 22:11:06 +0100

    fix: added further validation in auth middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,37 +2,54 @@ import { NextFunction, Request, Response } from "express";
 import { JwtPayload, verify } from "jsonwebtoken";
 import { HTTPCodes } from "../types/http_codes.enum";
 import { AccessTokenData } from "../types/token_data";
+import { IUserService } from "../interfaces/services";
 
-export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  (userService: IUserService) => {
     const token = req.headers.authorization;
 
     if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     try {
-        if (!process.env.ACCESS_TOKEN_SECRET) {
-            throw new Error("Access token secret is not defined");
-        }
+      if (!process.env.ACCESS_TOKEN_SECRET) {
+        throw new Error("Access token secret is not defined");
+      }
 
-        const rawTokenData = verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const rawTokenData = verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-        let tokenData: JwtPayload & AccessTokenData;
+      let tokenData: JwtPayload & AccessTokenData;
 
-        if (typeof rawTokenData === "string") {
-            tokenData = JSON.parse(rawTokenData);
-        } else {
-            tokenData = rawTokenData as JwtPayload & AccessTokenData;
-        }
+      if (typeof rawTokenData === "string") {
+        tokenData = JSON.parse(rawTokenData);
+      } else {
+        tokenData = rawTokenData as JwtPayload & AccessTokenData;
+      }
 
-        if (!tokenData.sub) {
-            throw new Error("Invalid token");
-        }
+      if (!tokenData.sub) {
+        throw new Error("Invalid token");
+      }
 
-        req.params.userId = tokenData.sub?.toString();
-        
-        next();
+      const userId = parseInt(tokenData.sub);
+      
+      const user = userService.findById(userId, userId);
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      req.params.userId = tokenData.sub?.toString();
+
+      next();
     } catch (error) {
-        return res.status(HTTPCodes.Unauthorized).json({ message: "Unauthorized" });
+      return res
+        .status(HTTPCodes.Unauthorized)
+        .json({ message: "Unauthorized" });
     }
-}
+  };
+};
